0c932e487fd87b315e37e19db52008dea593d8c0
Shubham Dokania <skd.1810@gmail.com>
2016-06-24 18:00:00 +0530

    Enter JaveScript

---
+++ static/js/class_03/main.js
@@ -0,0 +1,14 @@
+var x = 0;
+
+console.log("hello");
+
+updateValue = function () {
+    someText = document.getElementById('someText');
+}
+
+function button_clicked() {
+    p = document.getElementById('someText');
+    //console.log(p.text);
+    p.style.visibility = "hidden";
+
+}
