9aebf91b19fbbb3f807b08b942fd265c0013a4fa
Shubham Dokania <skd.1810@gmail.com>
2016-07-06 17:53:36 +0530

    add try file for moving box

--- class_08/static/js/main.js
+++ class_08/static/js/main.js
@@ -3,14 +3,9 @@
  */
 
 
-var storage = localStorage;
-
 function addItem() {
     var val = document.getElementById("data").value;
     var ul = document.getElementById("list");
-
-    var todos = storage.getItem("todos");
-    console.log(todos+['L']);
 
     if (val != "") {
         ul.innerHTML += '<li>' + val.toString() + '</li>';
