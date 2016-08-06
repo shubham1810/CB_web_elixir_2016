b4a86b02c2908670497d1b2195a502709bb96edb
Shubham Dokania <skd.1810@gmail.com>
2016-08-06 10:30:55 +0530

    Old commit

--- class_19/index.js
+++ class_19/index.js
@@ -10,5 +10,17 @@ MongoClient.connect(url, function (err, db) {
     }
     console.log("Connected correctly to the server");
 
+    let students = db.collection('students');
+    students.insertOne({
+        name: "shubham",
+        roll: "2K13/MC/082"
+    }, function (err, status) {
+        if (err != null) {
+            throw err;
+        }
+        console.log(status);
+    });
+
+
     db.close();
 });
